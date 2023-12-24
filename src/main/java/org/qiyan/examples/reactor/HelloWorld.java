fd5acae9aa81a2a3616aa0d3b180e2df793be31e
realqiyan <hongboyuan.yhb@gmail.com>
2023-12-24 13:00:41 +0800

    reactor

--- src/main/java/org/qiyan/examples/reactor/HelloWorld.java
+++ src/main/java/org/qiyan/examples/reactor/HelloWorld.java
@@ -2,10 +2,17 @@ package org.qiyan.examples.reactor;
 
 import reactor.core.publisher.Mono;
 
+import java.util.Arrays;
+
 public class HelloWorld {
     public static void main(String[] args) {
         Mono.just("hello")
                 .map(a -> a + "world")
                 .subscribe(System.out::println);
+
+        Arrays.asList("hello")
+                .stream()
+                .map(a -> a + "world")
+                .forEach(System.out::println);
     }
 }
