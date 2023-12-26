bb5c436df2c83f1b6651497939977ea250d9f9d0
realqiyan <hongboyuan.yhb@gmail.com>
2023-12-26 23:24:05 +0800

    a->(b1,b2,b3...)->c

--- src/main/java/org/qiyan/examples/flowapi/MergeProcessor.java
+++ src/main/java/org/qiyan/examples/flowapi/MergeProcessor.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.Flow;
 import java.util.concurrent.SubmissionPublisher;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 
@@ -15,11 +16,10 @@ import java.util.function.Function;
  */
 @Slf4j
 public class MergeProcessor extends SubmissionPublisher implements Flow.Processor {
-    private final int total;
-
+    private AtomicBoolean finish = new AtomicBoolean(false);
+    private int total;
     private List<Object> result;
     private Flow.Subscription subscription;
-
     public MergeProcessor(int total) {
         this.total = total;
         this.result = new CopyOnWriteArrayList<>();
@@ -35,7 +35,7 @@ public class MergeProcessor extends SubmissionPublisher implements Flow.Processo
     public void onNext(Object item) {
         result.add(item);
         log.info("merge:{}/{} result:{}", result.size(), total, item);
-        if (result.size() == total) {
+        if (result.size() == total && finish.compareAndExchange(false, true)) {
             submit(result);
         }
         subscription.request(Long.MAX_VALUE);
